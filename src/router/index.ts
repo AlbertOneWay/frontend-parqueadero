b0cefdcac30b3a0d5f6a80a3e405c7b016011126
Albert <albert.1701920351@ucaldas.edu.co>
2025-05-29 23:45:06 -0500

    views

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
-import RegisterVehicleView from '@/views/RegisterVehicleView.vue'
-import HistorialVehiculosView from '@/views/HistorialVehiculosView.vue'
-import RegistrarEventoAdmin from '@/views/RegistrarEventoAdmin.vue'
+import RegisterVehicleView from '../views/RegisterVehicleView.vue'
+import HistorialVehiculosView from '../views/HistorialVehiculosView.vue'
+import RegistrarEventoAdmin from '../views/RegistrarEventoAdmin.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
